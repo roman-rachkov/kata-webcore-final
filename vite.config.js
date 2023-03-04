e64c0304ee5dd29619d40ffeae30619b8222ff27
Roman Rachkov <pakycb84@gail.com>
2023-03-04 23:21:52 +1000

    configure vite

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+const path = require('path');
+export default defineConfig({
+  base: './',
+  publicDir: '/src/public',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
