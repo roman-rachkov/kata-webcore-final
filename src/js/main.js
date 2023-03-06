ae636545394383582d3aca46c56792a174a7aacb
Rachkov Roman <pakycb84@gmail.com>
2023-03-06 18:40:36 +1000

    Complete brands and hardware

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,16 @@
-import '@/scss/style.scss';
+import '@/scss/style.scss'
+
+document.querySelectorAll('.read-more').forEach((element) => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    const target = document.querySelector(event.target.dataset.target)
+
+    target.classList.toggle(event.target.dataset.class)
+    event.target.classList.toggle('read-more--active')
+
+    const text = event.target.dataset.text ?? 'Свернуть'
+    event.target.dataset.text = event.target.innerText
+    event.target.innerText = text
+  })
+})
