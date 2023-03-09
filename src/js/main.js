c0e828b82f789509a3f9716634628d25e28ab346
Rachkov Roman <pakycb84@gmail.com>
2023-03-09 18:44:22 +1000

    start addaptive

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,26 @@ document.querySelectorAll('.read-more').forEach((element) => {
     event.target.innerText = text
   })
 })
+
+document.querySelectorAll('.modal__close').forEach((element) => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault()
+    element.closest('.modal').close()
+  })
+})
+
+document.querySelectorAll('[data-modal]').forEach((element) => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault()
+    document.querySelector(element.dataset.modal).showModal()
+  })
+})
+document.querySelector('.menu__close').addEventListener('click', (event) => {
+  event.preventDefault()
+  event.target.closest('.menu').classList.remove('menu--active')
+})
+
+document.querySelector('.menu__show').addEventListener('click', (event) => {
+  event.preventDefault()
+  event.target.closest('.menu').classList.add('menu--active')
+})
